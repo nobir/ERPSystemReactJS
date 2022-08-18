6d46f9f3b5d0877d7b8d09740b1b7ac1aeee61de
Nobir <nobirfreelancer@gmail.com>
2022-08-18 18:20:29 +0600

    [Removed] some unused code

--- src/app/AxiosConfig.js
+++ src/app/AxiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ROUTES } from "./Routes";
+// import { ROUTES } from "./Routes";
 
 const Axios = axios.create({
     baseURL: "http://127.0.0.1:8000/api",
@@ -9,23 +9,6 @@ Axios.interceptors.request.use(
     (config) => {
         // do something before send the request
         // console.log("sending request");
-        switch (config.url) {
-            case ROUTES.home:
-            case ROUTES.about:
-                break;
-            case ROUTES.login:
-                if (
-                    localStorage.getItem("token") &&
-                    localStorage.getItem("token")
-                ) {
-                    window.location = ROUTES.dashboard;
-                }
-
-                break;
-
-            default:
-                break;
-        }
         return config;
     },
     (error) => {
