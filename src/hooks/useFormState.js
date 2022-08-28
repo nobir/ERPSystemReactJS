10532ded439c71a570c221eb4320e490fabb53e3
Nobir <nobirfreelancer@gmail.com>
2022-08-28 09:48:17 +0600

    [Fixed] select, checkbox and files not working properly

--- src/hooks/useFormState.js
+++ src/hooks/useFormState.js
@@ -2,13 +2,37 @@ import { useState } from "react";
 import useFormValidationError from "./useFormValidationError";
 
 function useFormState(initState = {}, initErrorState = {}) {
-    const { formValidationError, setFormValidationError } = useFormValidationError(initErrorState);
+    const { formValidationError, setFormValidationError } =
+        useFormValidationError(initErrorState);
     const [formState, setFormState] = useState(initState);
 
     const handleInputChange = (e) => {
-        e.preventDefault();
-        setFormState({ ...formState, [e.target.name]: e.target.value });
+        // console.log(e.target.multiple)
+
+        if (e.target.type === "file") {
+            e.preventDefault();
+            setFormState({ ...formState, [e.target.name]: e.target.files[0] });
+        } else if (e.target.tagName === "SELECT" && e.target?.multiple) {
+            e.preventDefault();
+            var options = e.target.options;
+            var value = [];
+            for (var i = 0, l = options.length; i < l; i++) {
+                if (options[i].selected) {
+                    value.push(+options[i].value);
+                }
+            }
+            setFormState({ ...formState, [e.target.name]: value });
+        } else if (e.target.type === "checkbox") {
+            setFormState({
+                ...formState,
+                [e.target.name]: e.target.checked,
+            });
+        } else {
+            e.preventDefault();
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
     };
+
     return {
         formValidationError,
         setFormValidationError,
